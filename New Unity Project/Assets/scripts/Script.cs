c907f7c08a99e386bc7cfe362d2db521f90c907c
VinayAero19 <57488399+VinayAero19@users.noreply.github.com>
2019-12-07 10:42:26 +0530

    Task-1

---
+++ New Unity Project/Assets/scripts/Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Script : MonoBehaviour
+{
+    private Animator anim;
+    private NavMeshAgent navMeshAgent;
+    private bool mrunning = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray,out hit,100))
+            {
+                navMeshAgent.destination = hit.point;
+            }
+        }
+
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            mrunning = false;
+        }
+        else
+            mrunning = true;
+        anim.SetBool("running", mrunning);
+    }
+}
