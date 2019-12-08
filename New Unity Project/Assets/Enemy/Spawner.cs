8c2cee0db475cd15a60defd58b3aeaa10d85e8bf
VinayAero19 <57488399+VinayAero19@users.noreply.github.com>
2019-12-08 12:24:23 +0530

    updated

---
+++ New Unity Project/Assets/Enemy/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] spawners;
+    public GameObject enemy;
+    void Start()
+    {
+        spawners = new GameObject[5];
+        for(int i =0;i<spawners.Length;i++)
+        {
+            spawners[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.T))
+        {
+            spawnEnemy();
+        }
+    }
+    private void spawnEnemy()
+    {
+        int spawnerId = Random.Range(0, spawners.Length);
+        Instantiate(enemy, spawners[spawnerId].transform.position, spawners[spawnerId].transform.rotation);
+    }
+}
