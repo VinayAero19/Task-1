b5af5b0c21d3f35d2910cd3a794897282aa95c18
VinayAero19 <57488399+VinayAero19@users.noreply.github.com>
2019-12-09 13:25:48 +0530

    update

---
+++ New Unity Project/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform goal;
+    private Animator anim;
+    private bool mrunning = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        var playerAgent = GetComponent<NavMeshAgent>();
+        playerAgent.destination = goal.position;
+        mrunning = true;
+        anim.SetBool("running", mrunning);
+
+        if(playerAgent.remainingDistance > playerAgent.stoppingDistance)
+        {
+            mrunning = false;
+            anim.SetBool("running", mrunning);
+        }
+
+    }
+
+    
+
+    
+
+
+    void Update()
+    {
+  
+
+    }
+
+}  
+    
+
