b21a2493651d6b569e0d5b086666888674395562
VinayAero19 <57488399+VinayAero19@users.noreply.github.com>
2019-12-26 18:26:48 +0530

    movement chnaged

---
+++ New Unity Project/Assets/Script/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    static Animator anim;
+    public float speed;
+    public float rotationSpeed;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float rotation = Input.GetAxis("Horizontal")* rotationSpeed;
+        float translation = Input.GetAxis("Vertical") * speed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        if(translation !=0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
